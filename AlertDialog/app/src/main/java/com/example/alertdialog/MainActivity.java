28f7898a8873ffd8804ad40f4f71b3913c06a8ab
huahuahuaX <864999506@qq.com>
2020-04-19 01:44:18 +0800

    AlertDialog
    Signed-off-by: huahuahuaX <864999506@qq.com>

---
+++ AlertDialog/app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void customView(View source){
+        TableLayout loginForm = (TableLayout) getLayoutInflater()
+                .inflate(R.layout.loginlayout, null);
+        new AlertDialog.Builder(this)
+                .setTitle("自定义对话框")
+                .setView(loginForm)
+                .setPositiveButton("登录", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .create()
+                .show();
+    }
+
+}
